ddbce9c45e84dc0f5ba55d98e349e3655d92fb34
winstonjoels <70131687+winstonjoels@users.noreply.github.com>
2020-08-29 10:32:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,18 +9,41 @@ from pycaret.regression import *
 import pandas as pd
 import pickle
 import numpy as np
+import random
+import csv
+
+def randN():
+    N=7
+    min = pow(10, N-1)
+    max = pow(10, N) - 1
+    id = random.randint(min, max)
+    return id
 
 app = Flask(__name__)
 
-global model, cols
+global model, cols, id
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
+@app.route('/eval',methods=['POST'])
+def eval():
+    filename = "data/Details.csv"
+    fields = [] 
+    rows = [] 
+    with open(filename, 'r') as csvfile: 
+        csvreader = csv.reader(csvfile) 
+        fields = next(csvreader) 
+        for row in csvreader: 
+            rows.append(row) 
+    df = pd.DataFrame(rows, columns = ['ID', 'Name', 'Predicted', 'Actual'])
+    return render_template("eval.html", tables=df, titles=df.columns.values)
+
 @app.route('/model/<name>',methods=['POST'])
 def model(name):
-    global model, cols
+    global model, cols, id
+    id = randN()
     cols = ['age','job','marital','education']
     file = name+".html"
     return render_template(file)
@@ -38,6 +61,15 @@ def predict(name):
         pred="Eligible for loan"
     else:
         pred="Not eligible for loan"
+    
+    actual = '?'
+    list = [id, name, int(prediction), actual]
+    list2 = ['ID','Name', 'Predicted', 'Actual']
+    df = np.array(list)
+    df = pd.DataFrame([df], columns=list2)
+    
+    df.to_csv('data/Details.csv', mode='a', header=False, index=False)
+    
     file = name+".html"
     return render_template(file,pred='{}'.format(pred))
 
